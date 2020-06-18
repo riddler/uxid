2981ee47a8081ac22984dc24a47468a5092cf1c2
JohnnyT <ubergeek3141@gmail.com>
2020-06-17 21:17:50 -0600

    Prepares UXID v0.0.1 (RB)
    This prepares to publish v0.0.1 to RubyGems.

--- impl/rb/test/uxid_test.rb
+++ impl/rb/test/uxid_test.rb
@@ -1,11 +1,7 @@
 require "test_helper"
 
-class UxidTest < Minitest::Test
+class UXIDTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Uxid::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+    refute_nil ::UXID::VERSION
   end
 end
