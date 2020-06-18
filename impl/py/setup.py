51b2c18d4defcdc26db5cfeccc2e9c3e31dae9a5
JohnnyT <ubergeek3141@gmail.com>
2020-06-17 21:46:50 -0600

    Uses PyPI registry
    The code had a remnant of testing - using the test PyPI registry. This
    fixes the code to use the correct registry.

--- impl/py/setup.py
+++ impl/py/setup.py
@@ -89,7 +89,7 @@ class UploadCommand(Command):
         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
 
         self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload --repository testpypi dist/*')
+        os.system('twine upload dist/*')
 
         # self.status('Pushing git tags…')
         # os.system('git tag v{0}'.format(about['__version__']))
