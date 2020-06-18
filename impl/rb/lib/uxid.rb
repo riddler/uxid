f9f20f8b8f92bb650b4b915de288d1f56e0dc046
JohnnyT <ubergeek3141@gmail.com>
2020-06-17 23:04:54 -0600

    [RB] Implements initial specs
    This implements the initial specs in Ruby.
    
    Relates #9

--- impl/rb/lib/uxid.rb
+++ impl/rb/lib/uxid.rb
@@ -1,5 +1,21 @@
 require "uxid/version"
 
+CROCKFORD_ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+INVALID_REGEX = /[^#{CROCKFORD_ENCODING}]/
+
 module UXID
   class Error < StandardError; end
+  
+  def self.decode value
+    case value
+    when String then
+      if value.strip.size == 0
+        raise "input is required"
+
+      elsif value =~ INVALID_REGEX
+        raise "expected input to be a Base32 encoded string, got: '#{value}'"
+      end
+    end
+    "uxid"
+  end
 end
